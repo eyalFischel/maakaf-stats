59ffd7a0887ba03d7c57c2e3ef6ff78885c921fd
Aviv <AvivRahamim95@gmail.com>
2024-06-04 20:36:59 +0300

    member activity implemeted

--- airflow/scripts/discord_scripts/discord_bot.py
+++ airflow/scripts/discord_scripts/discord_bot.py
@@ -16,19 +16,42 @@ client = discord.Client(intents=intents)
 
 join_count = 0
 
+# key: channel_name, value: [message_count, last_message_time] 
+project_channels = {}
+# key: member_name, value: {key: channel_name, val: [message_count, last_message_time]}
+member_activity = {}
+
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
 
     asyncio.create_task(reset_counters_weekly())
+    
+    guild = client.get_guild(GUILD_ID)
+    if not guild:
+        print("guild not found")
+    
+    project_categories = [c for c in guild.categories if is_projects_category(c)]
 
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    author = message.author
+    channel = message.channel
+    if message.author.bot:
         return
     
+    if author.name not in member_activity:
+        member_activity[author.name] = {}
+    
+    if channel.name not in member_activity[author.name]:
+        member_activity[author.name][channel.name] = [0, 0]
+    
+    member_activity[author.name][channel.name][0] += 1
+    member_activity[author.name][channel.name][1] = message.created_at.strftime('%Y-%m-%d %H:%M:%S')
+    print(member_activity)
 
+    
 @client.event
 async def on_member_join(member):
     if member.guild.id == GUILD_ID:
@@ -53,6 +76,13 @@ async def reset_counters_weekly() -> None:
 
         print("Weekly counter reset " + datetime.now())
         await asyncio.sleep(7*24*60*60)
+
+
+def is_projects_category(category):
+    return category.name.lower().startswith('projects-')
+
+def is_text_channel(channel):
+    return channel.type.value == 0
 
 
 client.run(BOT_TOKEN)
