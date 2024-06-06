fcdf6934aac65af6ae5225ec5bfe064223113d94
Aviv <AvivRahamim95@gmail.com>
2024-06-06 17:55:50 +0300

    added function types, requirements.txt and refractored for class structure

--- airflow/scripts/discord_scripts/utils.py
+++ airflow/scripts/discord_scripts/utils.py
@@ -1,11 +1,12 @@
+import discord
 
-def is_projects_category(category):
+def is_projects_category(category: discord.CategoryChannel) -> bool:
     return category.name.lower().startswith('projects-')
 
-def is_text_channel(channel):
+def is_text_channel(channel: discord.channel) -> bool:
     return channel.type.value == 0
 
-def update_member_activity(message, member_activity):
+def update_member_activity(message: discord.message, member_activity: dict) -> None:
     author = message.author
     channel = message.channel
 
@@ -19,7 +20,7 @@ def update_member_activity(message, member_activity):
     member_activity[author.name][channel.name][1] = message.created_at.strftime('%Y-%m-%d %H:%M:%S')
     print(f'member activity: {member_activity}')
 
-def update_project_activity(message, project_activity):
+def update_project_activity(message: discord.message, project_activity: dict) -> None:
     channel_name = message.channel.name
     created_at = message.created_at
 
