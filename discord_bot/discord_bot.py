38fc4d5912f7c2eb7402dc89a07f2813177fe056
Aviv <AvivRahamim95@gmail.com>
2024-06-24 08:52:45 +0300

    code clean up and formatting

--- discord_bot/discord_bot.py
+++ discord_bot/discord_bot.py
@@ -1,29 +1,23 @@
-# import asyncio
-# from datetime import datetime, timedelta
+"""discord bot that tracks guild member activity and save it on db"""
+
 import os
 
 import discord
 from dotenv import load_dotenv
 
-from utils import *
-from discord_db.modules import *
 from discord_db.db import Session
+from utils import insert_user, insert_channel, insert_message
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 
 class DiscordBot(discord.Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # self.join_count: int = 0
-        # # key: channel_name, value: [message_count, last_message_time] 
-        # self.project_activity: dict[str,list] = {}
-        # # key: member_name, value: {key: channel_name, val: [message_count, last_message_time]}
-        # self.member_activity: dict[str,dict[str,list]] = {}
-
+    """class for the discord"""
 
     async def on_ready(self) -> None:
+        """called when the bot starts running, scans the db and inserts missing channels and users"""
         print(f"We have logged in as {client.user}")
 
         with Session() as session:
@@ -32,51 +26,32 @@ class DiscordBot(discord.Client):
             for user in client.guilds[0].members:
                 insert_user(session, user.name, user.joined_at)
 
-        # asyncio.create_task(self.reset_counters_weekly())
-
-
     async def on_message(self, message: discord.message) -> None:
+        """adds the message info to the db"""
         if message.author.bot or message.channel.type.value != 0:
             return
-        
-        with Session() as session:
-            insert_message(session, str(message.id), str(message.channel.id), message.author.name, message.created_at)
-        # update_member_activity(message, self.member_activity)
-        # if message.channel.category and is_projects_category(message.channel.category):
-        #     update_project_activity(message, self.project_activity)
 
+        with Session() as session:
+            insert_message(
+                session,
+                str(message.id),
+                str(message.channel.id),
+                message.author.name,
+                message.created_at,
+            )
 
     async def on_guild_channel_create(self, channel: discord.abc.GuildChannel) -> None:
-            with Session() as session:
-                insert_channel(session, str(channel.id), channel.name)
-
+        """adds the new guild channel to the db"""
+        with Session() as session:
+            insert_channel(session, str(channel.id), channel.name)
 
     async def on_member_join(self, member: discord.member) -> None:
+        """adds the new member to the db"""
         with Session() as session:
             insert_user(session, member.name, member.joined_at)
-        # self.join_count += 1
-        # print(member.name + " joined. " + f"{self.join_count} new members has joined this week")
 
 
-    # async def reset_counters_weekly(self) -> None:
-    #     await client.wait_until_ready()
-    #     while not client.is_closed():
-    #         now = datetime.now()
-    #         days_until_next_sunday = (6 - now.weekday()) if now.weekday() != 6 else 7
-    #         next_reset = now + timedelta(days=days_until_next_sunday)
-    #         next_reset = next_reset.replace(hour=0, minute=0, second=0, microsecond=0)
-    #         wait_time = (next_reset - now).total_seconds()
-
-    #         print(f"next counters reset: {next_reset}")
-    #         await asyncio.sleep(wait_time)
-
-    #         self.join_count = 0
-
-    #         print("Weekly counter reset " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    #         await asyncio.sleep(7*24*60*60)
-
-if __name__ == "__main__":
-    intents = discord.Intents.default()
-    intents.members = True
-    client = DiscordBot(intents=intents)
-    client.run(BOT_TOKEN)
+intents = discord.Intents.default()
+intents.members = True
+client = DiscordBot(intents=intents)
+client.run(BOT_TOKEN)
--- discord_bot/utils.py
+++ discord_bot/utils.py
@@ -1,62 +1,44 @@
+"""helper functions for the discord bot"""
+
 from datetime import datetime
 
-import discord
 from sqlalchemy import select
 
 
-from discord_db.modules import *
-# from db.discord_db.db import Session
+from discord_db.modules import User, Channel, Message
+
 
 def insert_user(session, username: str, joined_at: datetime) -> None:
-    stmt = select(User).where(User.username==username)
+    """insers a user to the db"""
+    stmt = select(User).where(User.username == username)
     if session.scalars(stmt).first():
         return
-    
+
     user = User(username=username, joined_at=joined_at)
     session.add(user)
     session.commit()
 
+
 def insert_channel(session, channel_id: str, name: str) -> None:
-    stmt = select(Channel).where(Channel.channel_id==channel_id)
+    """inserts a channel to the db"""
+    stmt = select(Channel).where(Channel.channel_id == channel_id)
     if session.scalars(stmt).first():
         return
-    
+
     channel = Channel(channel_id=channel_id, name=name)
     session.add(channel)
     session.commit()
 
-def insert_message(session, message_id: str, channel_id: str, username: str, created_at: datetime) -> None:
-    message = Message(message_id=message_id, channel_id=channel_id, username=username, created_at=created_at)
+
+def insert_message(
+    session, message_id: str, channel_id: str, username: str, created_at: datetime
+) -> None:
+    """inserts a message to the db"""
+    message = Message(
+        message_id=message_id,
+        channel_id=channel_id,
+        username=username,
+        created_at=created_at,
+    )
     session.add(message)
     session.commit()
-
-def is_projects_category(category: discord.CategoryChannel) -> bool:
-    return category.name.lower().startswith('projects-')
-
-def is_text_channel(channel: discord.channel) -> bool:
-    return channel.type.value == 0
-
-def update_member_activity(message: discord.message, member_activity: dict[str,dict[str,list]]) -> None:
-    author = message.author
-    channel = message.channel
-
-    if author.name not in member_activity:
-        member_activity[author.name] = {}
-    
-    if channel.name not in member_activity[author.name]:
-        member_activity[author.name][channel.name] = [0, 0]
-    
-    member_activity[author.name][channel.name][0] += 1
-    member_activity[author.name][channel.name][1] = message.created_at.strftime('%Y-%m-%d %H:%M:%S')
-    print(f'member activity: {member_activity}')
-
-def update_project_activity(message: discord.message, project_activity: dict[str,list]) -> None:
-    channel_name: str = message.channel.name
-    created_at = message.created_at
-
-    if channel_name not in project_activity:
-        project_activity[channel_name] = [0, 0]
-    
-    project_activity[channel_name][0] += 1
-    project_activity[channel_name][1] = created_at.strftime('%Y-%m-%d %H:%M:%S')
-    print(f'project activity: {project_activity}')
