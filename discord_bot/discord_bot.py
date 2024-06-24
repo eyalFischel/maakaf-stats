8bb29e75eb53b60f428e554af93e9733d3721c77
Aviv <AvivRahamim95@gmail.com>
2024-06-24 07:29:02 +0300

    added functionality to update discord db

--- discord_bot/discord_bot.py
+++ discord_bot/discord_bot.py
@@ -1,11 +1,13 @@
-import asyncio
-from datetime import datetime, timedelta
+# import asyncio
+# from datetime import datetime, timedelta
 import os
 
 import discord
 from dotenv import load_dotenv
 
 from utils import *
+from discord_db.modules import *
+from discord_db.db import Session
 
 load_dotenv()
 
@@ -14,49 +16,58 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 class DiscordBot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.join_count: int = 0
-        # key: channel_name, value: [message_count, last_message_time] 
-        self.project_activity: dict[str,list] = {}
-        # key: member_name, value: {key: channel_name, val: [message_count, last_message_time]}
-        self.member_activity: dict[str,dict[str,list]] = {}
+        # self.join_count: int = 0
+        # # key: channel_name, value: [message_count, last_message_time] 
+        # self.project_activity: dict[str,list] = {}
+        # # key: member_name, value: {key: channel_name, val: [message_count, last_message_time]}
+        # self.member_activity: dict[str,dict[str,list]] = {}
 
 
     async def on_ready(self) -> None:
         print(f"We have logged in as {client.user}")
 
-        asyncio.create_task(self.reset_counters_weekly())
+        # asyncio.create_task(self.reset_counters_weekly())
 
 
     async def on_message(self, message: discord.message) -> None:
-        if message.author.bot:
+        if message.author.bot or message.channel.type.value != 0:
             return
         
-        update_member_activity(message, self.member_activity)
-        if message.channel.category and is_projects_category(message.channel.category):
-            update_project_activity(message, self.project_activity)
+        with Session() as session:
+            insert_message(session, message.id, message.channel.id, message.author.name, message.created_at)
+        # update_member_activity(message, self.member_activity)
+        # if message.channel.category and is_projects_category(message.channel.category):
+        #     update_project_activity(message, self.project_activity)
+
+
+    async def on_guild_channel_create(self, channel: discord.abc.GuildChannel) -> None:
+            with Session() as session:
+                insert_channel(session, channel.id, channel.name)
 
 
     async def on_member_join(self, member: discord.member) -> None:
-        self.join_count += 1
-        print(member.name + " joined. " + f"{self.join_count} new members has joined this week")
+        with Session() as session:
+            insert_user(session, member.name, member.joined_at)
+        # self.join_count += 1
+        # print(member.name + " joined. " + f"{self.join_count} new members has joined this week")
 
 
-    async def reset_counters_weekly(self) -> None:
-        await client.wait_until_ready()
-        while not client.is_closed():
-            now = datetime.now()
-            days_until_next_sunday = (6 - now.weekday()) if now.weekday() != 6 else 7
-            next_reset = now + timedelta(days=days_until_next_sunday)
-            next_reset = next_reset.replace(hour=0, minute=0, second=0, microsecond=0)
-            wait_time = (next_reset - now).total_seconds()
+    # async def reset_counters_weekly(self) -> None:
+    #     await client.wait_until_ready()
+    #     while not client.is_closed():
+    #         now = datetime.now()
+    #         days_until_next_sunday = (6 - now.weekday()) if now.weekday() != 6 else 7
+    #         next_reset = now + timedelta(days=days_until_next_sunday)
+    #         next_reset = next_reset.replace(hour=0, minute=0, second=0, microsecond=0)
+    #         wait_time = (next_reset - now).total_seconds()
 
-            print(f"next counters reset: {next_reset}")
-            await asyncio.sleep(wait_time)
+    #         print(f"next counters reset: {next_reset}")
+    #         await asyncio.sleep(wait_time)
 
-            self.join_count = 0
+    #         self.join_count = 0
 
-            print("Weekly counter reset " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-            await asyncio.sleep(7*24*60*60)
+    #         print("Weekly counter reset " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    #         await asyncio.sleep(7*24*60*60)
 
 if __name__ == "__main__":
     intents = discord.Intents.default()
--- discord_bot/utils.py
+++ discord_bot/utils.py
@@ -1,4 +1,34 @@
+from datetime import datetime
+
 import discord
+from sqlalchemy import select
+
+
+from discord_db.modules import *
+# from db.discord_db.db import Session
+
+def insert_user(session, username: str, joined_at: datetime) -> None:
+    stmt = select(User).where(User.username==username)
+    if session.scalars(stmt).first():
+        return
+    
+    user = User(username=username, joined_at=joined_at)
+    session.add(user)
+    session.commit()
+
+def insert_channel(session, channel_id: str, name: str) -> None:
+    stmt = select(Channel).where(Channel.channel_id==channel_id)
+    if session.scalars(stmt).first():
+        return
+    
+    channel = Channel(channel_id=channel_id, name=name)
+    session.add(channel)
+    session.commit()
+
+def insert_message(session, message_id: str, channel_id: str, username: str, created_at: datetime) -> None:
+    message = Message(message_id=message_id, channel_id=channel_id, username=username, created_at=created_at)
+    session.add(message)
+    session.commit()
 
 def is_projects_category(category: discord.CategoryChannel) -> bool:
     return category.name.lower().startswith('projects-')
