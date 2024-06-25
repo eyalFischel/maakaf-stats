adb41006046981666fe522021b2548db94297910
Aviv <AvivRahamim95@gmail.com>
2024-06-25 05:55:13 +0300

    now when the bot starts it updates the db, also update channel in the db when changed

--- discord_bot/discord_bot.py
+++ discord_bot/discord_bot.py
@@ -6,7 +6,7 @@ import discord
 from dotenv import load_dotenv
 
 from discord_db.db import Session
-from utils import insert_user, insert_channel, insert_message
+from utils import insert_user, insert_channel, insert_message, update_channel
 
 load_dotenv()
 
@@ -17,14 +17,27 @@ class DiscordBot(discord.Client):
     """class for the discord"""
 
     async def on_ready(self) -> None:
-        """called when the bot starts running, scans the db and inserts missing channels and users"""
+        """
+        called when the bot starts running,
+        scans the db and updates the data
+        """
         print(f"We have logged in as {client.user}")
 
         with Session() as session:
-            for channel in client.guilds[0].text_channels:
-                insert_channel(session, str(channel.id), channel.name)
             for user in client.guilds[0].members:
                 insert_user(session, user.name, user.joined_at)
+            for channel in client.guilds[0].text_channels:
+                insert_channel(session, str(channel.id), channel.name)
+                async for message in channel.history(limit=None):
+                    if message.author.bot:
+                        continue
+                    insert_message(
+                        session,
+                        str(message.id),
+                        str(message.channel.id),
+                        message.author.name,
+                        message.created_at,
+                    )
 
     async def on_message(self, message: discord.message) -> None:
         """adds the message info to the db"""
@@ -49,6 +62,12 @@ class DiscordBot(discord.Client):
         """adds the new member to the db"""
         with Session() as session:
             insert_user(session, member.name, member.joined_at)
+
+    async def on_guild_channel_update(self, before, after) -> None:
+        """updates the channel changes in the db"""
+        if before.name != after.name:
+            with Session() as session:
+                update_channel(session, str(after.id), after.name)
 
 
 intents = discord.Intents.default()
