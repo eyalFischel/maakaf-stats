2507de99c8232dde7a696a66f8b918d86baeab47
mario99logic <rohanamario@gmail.com>
2024-06-29 01:57:40 +0300

    connected between the github db  and the python app

--- airflow/scripts/git_scripts/main.py
+++ airflow/scripts/git_scripts/main.py
@@ -2,17 +2,44 @@ import os
 
 from dotenv import load_dotenv
 
+from database import SessionLocal
 from github_repository import RepositoryFetcher
 from repository import Repository
+from sql_modules import RepositoryORM, GitHubUserORM
 
 
 load_dotenv()
 github_token = os.getenv('GITHUB_TOKEN')
 
+
+def collect_repository_data() -> None:
+    session = SessionLocal()
+
+    # Fetch all repositories from the database
+    repositories = session.query(RepositoryORM).all()
+
+    for repo in repositories:
+        user = session.query(GitHubUserORM).filter(GitHubUserORM.user_id == repo.userid).first() # change from userid to username?
+        if user:
+            # Initialize RepositoryFetcher with owner and repo name
+            repository_info = {
+                'owner': user.username,
+                'name': repo.name
+            }
+            repository = Repository(owner=repository_info['owner'], name=repository_info['name'])
+            repo_fetcher = RepositoryFetcher(repository, github_token)
+
+            # Fetch repository activity
+            activity = repo_fetcher.fetch_repository_activity()
+            print(f"Activity for {repository_info['owner']}/{repository_info['name']}: {activity}")
+
+    session.close()
+
 def main():
-    repository1 = Repository(owner='eyalFischel', name='maakaf-stats')
-    repo1 = RepositoryFetcher(repository1, github_token)
-    print(repo1.fetch_repository_activity())
+    collect_repository_data()
+  #  repository1 = Repository(owner='eyalFischel', name='maakaf-stats')
+  #  repo1 = RepositoryFetcher(repository1, github_token)
+  #  print(repo1.fetch_repository_activity())
 
 
 main()
---
+++ airflow/scripts/git_scripts/sql_modules.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, mapped_column
+
+Base = declarative_base()
+
+class GitHubUserORM(Base):
+    __tablename__ = 'githubuser'
+
+    user_id = mapped_column(Integer, primary_key=True, index=True)
+    username = mapped_column(String(39), unique=True)
+    email = mapped_column(String(254), unique=True)
+
+    repositories = relationship("RepositoryORM", back_populates="user")
+
+
+class RepositoryORM(Base):
+    __tablename__ = 'repository'
+
+    repoid = mapped_column(Integer, primary_key=True, index=True)
+    name = mapped_column(String(255))
+    forks = mapped_column(Integer)
+    stars = mapped_column(Integer)
+    commits = mapped_column(Integer)
+    pullrequests = mapped_column(Integer)
+    issues = mapped_column(Integer)
+    comments = mapped_column(Integer)
+    watchers = mapped_column(Integer)
+    views = mapped_column(Integer)
+    activeusers = mapped_column(Integer)
+    userid = mapped_column(Integer, ForeignKey('githubuser.user_id'))
+    fetched_at = mapped_column(DateTime, index=True)
+
+    user = relationship("GitHubUserORM", back_populates="repositories")
